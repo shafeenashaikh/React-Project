ea54fa198582defd52184395eec4b79c59924d85
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-09-23 12:24:49 +0530

    added header to all pages

--- src/Components/AddProduct.js
+++ src/Components/AddProduct.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from './Header'
+
 
 function AddProduct(){
     return(
         <div>
+            <Header/>
             <h1>Add Product</h1>
         </div>
     )
