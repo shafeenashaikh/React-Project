22b1e1f5ef599e2e2332282bdeaed354f8689060
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-09-24 10:57:33 +0530

    created product list

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,10 +1,49 @@
 import React from 'react';
 import Header from './Header';
+import {Table} from 'react-bootstrap'
 function ProductList(){
+    
     return(
         <div>
             <Header/>
+            <div className="col-sm-8 offset-sm-2">
             <h2>Product list</h2>
+    <Table striped bordered hover>
+        <thead>     
+            <tr>
+             <th>#</th>
+            <th>Name</th>
+            <th>Image</th>
+            <th>Description</th>
+            <th>Price</th>
+            
+            </tr>
+         </thead>
+         <tbody>
+            <tr>
+                <td>1</td>
+                <td>one-plus</td>
+                <td><img src="C:\Users\intel\Desktop\1.jpg"/></td>
+                <td>Quality product</td>
+                <td>70000</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>I-phone</td>
+                <td><img src="C:\Users\intel\Desktop\1.jpg"/></td>
+                <td>Apple product</td>
+                <td>60000</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>sumsang</td>
+                <td><img src="C:\Users\intel\Desktop\1.jpg"/></td>
+                <td>Quality product</td>
+                <td>10000</td>
+            </tr>
+        </tbody>
+    </Table>
+    </div> 
         </div>
     )
 }
