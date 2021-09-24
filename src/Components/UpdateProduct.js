fa82773dd92441b857be6935cf1aa6891f84b383
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-09-24 12:07:25 +0530

    created prefill data form for updateproduct

--- src/Components/UpdateProduct.js
+++ src/Components/UpdateProduct.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Header from './Header'
-
-function UpdateProduct(){
+import {withRouter} from 'react-router-dom'
+import { useState } from 'react'
+function UpdateProduct(props){
+    console.log("props",props)
+    const[name,setName]=useState("sumsang")
+    const[price,setPrice]=useState("price")
+    const[description,setDescription]=useState("description")
+   
     return(
         <div>
             <Header/>
             <h1>Update Product</h1>
+            <input type="text" defaultValue={name}/> <br/><br/>
+            <input type="text" defaultValue={price}/> <br/><br/>
+            <input type="text" defaultValue={description}/> <br/><br/>
+            <input type="file" defaultValue={""}/> <br/><br/>
+            <button>Update</button>
         </div>
     )
 }
-export default UpdateProduct
+export default withRouter(UpdateProduct)
