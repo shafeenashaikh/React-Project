29e436093c50501f3acfb4ab459c72fd203fc5a9
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-09-24 14:08:52 +0530

    updating product

--- src/Components/UpdateProduct.js
+++ src/Components/UpdateProduct.js
@@ -4,19 +4,26 @@ import {withRouter} from 'react-router-dom'
 import { useState } from 'react'
 function UpdateProduct(props){
     console.log("props",props)
+
     const[name,setName]=useState("sumsang")
     const[price,setPrice]=useState("price")
     const[description,setDescription]=useState("description")
+    const[file,setFile]=useState("")
+
+
+    function editproduct(id){
+        alert(id)
+    }
    
     return(
         <div>
             <Header/>
             <h1>Update Product</h1>
-            <input type="text" defaultValue={name}/> <br/><br/>
-            <input type="text" defaultValue={price}/> <br/><br/>
-            <input type="text" defaultValue={description}/> <br/><br/>
-            <input type="file" defaultValue={""}/> <br/><br/>
-            <button>Update</button>
+            <input type="text" onChange={(e)=>setName(e.target.value)} defaultValue={name}/> <br/><br/>
+            <input type="text" onChange={(e)=>setPrice(e.target.value)} defaultValue={price}/> <br/><br/>
+            <input type="text" onChange={(e)=>setDescription(e.target.value)} defaultValue={description}/> <br/><br/>
+            <input type="file" onChange={(e)=>setFile(e.target.value)} defaultValue={file}/> <br/><br/>
+            <button onClick={editproduct}>Update</button>
         </div>
     )
 }
